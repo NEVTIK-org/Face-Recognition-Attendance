e8db28629f8dbf53216983147625ef9045b1a691
dark-hermes <hermawansent@gmail.com>
2021-04-06 23:17:28 +0700

    Initial commit

---
+++ attendance.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+
+time = datetime.now().strftime("%H:%M")
+
+
+def attendance_csv(name, time=time):
+    with open("Attendance.csv", "r+") as file:
+        attendance_data = file.readlines()
+        existing_name = [data.split(',')[0] for data in attendance_data]
+        if name not in existing_name:
+            file.writelines(f"\n{name},{time}")
